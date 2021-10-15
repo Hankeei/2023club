4c8877c8ce9db5bb3a38fcd891b923d15bd9a759
thefatau <92589232+thefatau@users.noreply.github.com>
2021-10-15 22:50:09 +0800

    Create D301.cpp

---
+++ HKOI/thefatau's/DSE Exercises/D301.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string name1 , name2 , food , trans , place;
+  getline(cin , name1);
+  getline(cin , name2);
+  getline(cin , food);
+  getline(cin , trans);
+  getline(cin , place);
+  cout << name1 << " " << name2 << " " << food << " " << trans << " " << place << endl;
+  return 0;
+}
