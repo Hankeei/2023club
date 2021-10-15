40f6e2981dfd8136739b45f6420523bbf37d60b0
thefatau <92589232+thefatau@users.noreply.github.com>
2021-10-15 22:45:47 +0800

    Create D108.cpp

---
+++ HKOI/thefatau's/DSE Exercises/D108.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a , b , c;
+  char x , y;
+  cin >> a >> x >> b >> y >> c;
+  if (x == '+' && y == '+') {
+    cout << a + b + c;
+  }
+  else if (x == '+' && y == '-') {
+    cout << a + b - c;
+  }
+  else if (x == '+' && y == '*') {
+    cout << a + b * c;
+  }
+  else if (x == '-' && y == '+') {
+    cout << a - b + c;
+  }
+  else if (x == '-' && y == '-') {
+    cout << a - b - c;
+  }
+  else if (x == '-' && y == '*') {
+    cout << a - b * c;
+  }
+  else if (x == '*' && y == '+') {
+    cout << a * b + c;
+  }
+  else if (x == '*' && y == '-') {
+    cout << a * b - c;
+  }
+  else if (x == '*' && y == '*') {
+    cout << a * b * c;
+  }
+  
+  return 0;
+}
