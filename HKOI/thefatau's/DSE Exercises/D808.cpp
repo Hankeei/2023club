9cdc4eb1cf3f6b4a2784346b69e495fcca27e0c3
thefatau <92589232+thefatau@users.noreply.github.com>
2021-10-22 23:21:52 +0800

    Create D808.cpp

---
+++ HKOI/thefatau's/DSE Exercises/D808.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sum = 0;
+int partition (int arr[], int st, int end) {
+  sum += (end - st + 1);
+  int pivot = arr[end];
+  int i = st, d = 0;
+  for (int j = st; j < end; j++) {
+    if (arr[j] <= pivot) {
+      d = arr[j];
+      arr[j] = arr[i];
+      arr[i] = d;
+      i++;
+    }
+  }
+  d = arr[i];
+  arr[i] = arr[end];
+  arr[end] = d;
+  return i;
+}
+
+void quicksort (int arr[], int st, int end) {
+  if (st <= end) {
+    int p = partition(arr, st, end);
+    quicksort(arr, st, p - 1);
+    quicksort(arr, p + 1, end);
+  }
+}
+
+int main() {
+  int n, i = 0;
+  cin >> n;
+  int arr[100000];
+  while (cin >> arr[i++]);
+  quicksort(arr, 0, n - 1);
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+  cout << sum;
+  return 0;
+}
