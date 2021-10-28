3b3aae9a876eae78a8abe6e6f051e57c4408c818
thefatau <92589232+thefatau@users.noreply.github.com>
2021-10-29 02:20:01 +0800

    Create M17A1.cpp

---
+++ HKOI/thefatau's/Mini-comp/M17A1.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  switch(n){
+    case 2017:
+        cout << "53 35 26" << endl;
+        break;
+    case 2016:
+        cout << "55 21 13" << endl;
+        break;
+    case 2015:
+        cout << "42 39 9" << endl;
+        break;
+    case 2014:
+        cout << "36 32 8" << endl;
+        break;
+    case 2013:
+        cout << "31 31 26" << endl;
+        break;
+    case 2012:
+        cout << "37 20 17" << endl;
+        break;
+    case 2011:
+        cout << "32 25 23" << endl;
+        break;
+    case 2010:
+        cout << "28 20 16" << endl;
+        break;
+    case 2009:
+        cout << "36 28 13" << endl;
+        break;
+    case 2008:
+        cout << "30 29 11" << endl;
+        break;
+    case 2007:
+        cout << "41 19 8" << endl;
+        break;
+    case 2006:
+        cout << "40 22 13" << endl;
+        break;
+    case 2005:
+        cout << "25 17 12" << endl;
+        break;
+    case 2004:
+        cout << "19 15 13" << endl;
+        break;
+    case 2003:
+        cout << "18 12 12" << endl;
+        break;
+    case 2002:
+        cout << "17 13 8" << endl;
+        break;
+    case 2001:
+        cout << "13 11 10" << endl;
+        break;
+    case 2000:
+        cout << "14 9 8" << endl;
+        break;
+  }
+  return 0;
+}
