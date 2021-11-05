722b235784566bbfb6b60c29e36a774072601db8
thefatau <92589232+thefatau@users.noreply.github.com>
2021-11-06 01:05:57 +0800

    Update M17A5.cpp

--- HKOI/thefatau's/Mini-comp/M17A5.cpp
+++ HKOI/thefatau's/Mini-comp/M17A5.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-  int a, b;
+  int a;
   cin >> a;
-  while (cin >> b) {
-    if (b % 2 == 1) {
-      cout << b;
+  while (cin >> a) {
+    if (a % 2 == 1) {
+      cout << a;
       return 0;
     }
   }
